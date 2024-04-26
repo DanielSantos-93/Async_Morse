2b1e38e7900aaec841debad75b5715695d4448ee
DanielSantos-93 <146108950+DanielSantos-93@users.noreply.github.com>
2024-04-25 22:14:27 -0300

    Update Async_Morse_2_Leds.ino
    Improvement in comments

--- examples/Async_Morse_2_Leds/Async_Morse_2_Leds.ino
+++ examples/Async_Morse_2_Leds/Async_Morse_2_Leds.ino
@@ -12,14 +12,14 @@ Morse morse2(4, 200, 1000, 200, 1000, 2000, 3000);
 
 void setup() {
   Serial.begin(115200);
-  morse1.palavra("Daniel"); // Define a palavra "Daniel" para o primeiro objeto Morse, sem repetição
-  morse2.palavra("SOS", true); // Define a palavra "Daniel" para o primeiro objeto Morse, com repetição
+  morse1.palavra("Daniel"); // Define a palavra "Daniel" para o objeto morse1, sem repetição
+  morse2.palavra("SOS", true); // Define a palavra "Daniel" para o objeto morse2, com repetição
 }
 
 void loop() {
   morse1.ativar(true); // O objeto morse1 irá ser ativado com debug Serial
   morse2.ativar(); // O objeto morse2 irá ser ativado sem debug Serial
   if (morse1.finalMorse) { // Caso a palavra do objeto morse1 finalize
-    morse1.palavra("ESP32", true); // Modifique a palavra do objeto morse1 para "ESP32" com debug Serial
+    morse1.palavra("ESP32", true); // Modifique a palavra do objeto morse1 para "ESP32" com repetição
   }
 }
